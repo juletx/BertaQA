7c30b4a26f14093a9394671067c2d2a77fab9d68
Julen Etxaniz <juletxara@gmail.com>
2024-02-24 16:04:32 +0100

    update dataset translation code

--- translate/dataset_configs.py
+++ translate/dataset_configs.py
@@ -274,4 +274,21 @@ dataset_configs = {
             "default": "Euskara",
         },
     },
+    "basquetrivia": {
+        "dataset": "HiTZ/BasqueTrivia",
+        "dataset_configs": ["eu"],
+        "dataset_split": "test",
+        "dataset_fields": [
+            "question",
+            "candidates",
+        ],
+        "file_path": "../datasets/eustrivia_mt",
+        "filename": "{config}.jsonl",
+        "lang_codes": {
+            "eu": "eus_Latn",
+        },
+        "lang_names": {
+            "eu": "Euskara",
+        },
+    },
 }
